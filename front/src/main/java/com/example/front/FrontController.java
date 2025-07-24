4d6c90adfb7d9fbdb68a32868a4eb0818d052d56
배지한 <imganchi@gmail.com>
2025-07-24 14:10:04 +0900

    cicd v4

--- front/src/main/java/com/example/front/FrontController.java
+++ front/src/main/java/com/example/front/FrontController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class FrontController {
