4e21420d06f3ac573ffa9b091af3f5c9be6cc6ad
배지한 <imganchi@gmail.com>
2025-07-24 16:22:40 +0900

    fix

---
+++ front/target/classes/com/example/front/FrontController.java
@@ -0,0 +1,34 @@
+package com.example.front;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+@Controller
+public class FrontController {
+    @Value("${api.url}")
+    private String apiUrl;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Integer count = restTemplate.getForObject(apiUrl, Integer.class);
+        model.addAttribute("count", count);
+        return "index";
+    }
+
+    @PostMapping("/inc1")
+    public String inc1() {
+        restTemplate.postForObject(apiUrl + "/inc1", null, Integer.class);
+        return "redirect:/";
+    }
+
+    @PostMapping("/inc2")
+    public String inc2() {
+        restTemplate.postForObject(apiUrl + "/inc2", null, Integer.class);
+        return "redirect:/";
+    }
+} 
